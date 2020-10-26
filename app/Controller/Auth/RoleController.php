6dac1e2d2a8a3e332eefe32ca7b3606c470c67ee
linyiyuan <linyiyuann@gmail.com>
2020-10-26 14:35:31 +0800

    '完善角色控制器'

--- app/Controller/Auth/RoleController.php
+++ app/Controller/Auth/RoleController.php
@@ -40,7 +40,7 @@ class RoleController extends AbstractController
         $total = $roleQuery->count();
         $roleQuery = $this->pagingCondition($roleQuery, $this->request->all());
         //判断是否有查询条件
-        if(!empty( $this->request->input('description'))) $roleQuery->where('description', 'like', '%' . $this->request->input('description') . '%');
+        if (!empty($this->request->input('description'))) $roleQuery->where('description', 'like', '%' . $this->request->input('description') . '%');
         $list = $roleQuery->get();
 
         return $this->success([
@@ -56,56 +56,82 @@ class RoleController extends AbstractController
      */
     public function store()
     {
-            $postData = $this->request->all();
-            $params = [
-                'name' => $postData['name'] ?? '',
-                'description' => $postData['description'] ?? '',
-            ];
-            //配置验证
-            $rules = [
-                'name' => 'required',
-            ];
-            $message = [
-                'name.required' => '[name]缺失',
-            ];
-            $this->verifyParams($params, $rules, $message);
+        $postData = $this->request->all();
+        $params = [
+            'name' => $postData['name'] ?? '',
+            'description' => $postData['description'] ?? '',
+        ];
+        //配置验证
+        $rules = [
+            'name' => 'required',
+        ];
+        $message = [
+            'name.required' => '[name]缺失',
+        ];
+        $this->verifyParams($params, $rules, $message);
 
-            if (!Role::create($params)) $this->throwExp(400, '添加角色失败');
+        if (!Role::create($params)) $this->throwExp(400, '添加角色失败');
 
-            return $this->successByMessage('添加角色成功');
+        return $this->successByMessage('添加角色成功');
     }
 
     /**
      * 修改角色
-     * @RequestMapping(path="update", methods="put")
+     * @param int $id
+     * @RequestMapping(path="update/{id}", methods="put")
      * @Middleware(RequestMiddleware::class)
+     * @return \Psr\Http\Message\ResponseInterface
      */
-    public function update()
+    public function update(int $id)
     {
-        try {
-            if (!intval($id)) $this->throwExp('未知id参数');
+        $postData = $this->request->all();
+        $params = [
+            'id' => $id,
+            'name' => $postData['name'],
+            'description' => $postData['description']
+        ];
+        //配置验证
+        $rules = [
+            'id' => 'required',
+            'name' => 'required',
+        ];
+        $message = [
+            'id.required' => '非法参数',
+            'name.required' => '[name]缺失',
+        ];
 
-            $postData = $request->postData;
-            $params = [
-                'name'          => $postData['name'],
-                'description'   => $postData['description']
-            ];
-            //配置验证
-            $rules = [
-                'name' => 'required',
-            ];
-            $message = [
-                'name.required' => '[name]缺失',
-            ];
+        $this->verifyParams($params, $rules, $message);
 
-            $this->verifyParams($params, $rules, $message);
+        if (!Role::query()->where('id', $id)->update($params)) $this->throwExp(400, '修改角色信息失败');
 
-            if (!Role::query()->where('id', $id)->update($params)) $this->throwExp('400','修改角色信息失败');
-
-            //正确返回信息
-            return $this->success('', '修改角色信息成功');
-        } catch (Exception $e) {
-            return $this->errorExp($e);
-        }
+        return $this->successByMessage('修改角色信息成功');
     }
+
+    /**
+     * 修改角色
+     * @param int $id
+     * @RequestMapping(path="destroy/{id}", methods="delete")
+     * @Middleware(RequestMiddleware::class)
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function destroy(int $id)
+    {
+        $params = [
+            'id' => $id,
+        ];
+        //配置验证
+        $rules = [
+            'id' => 'required',
+        ];
+        $message = [
+            'id.required' => '非法参数',
+        ];
+
+        $this->verifyParams($params, $rules, $message);
+
+        if (!Role::query()->where('id', $id)->delete()) $this->throwExp(400, '删除角色信息失败');
+
+        return $this->successByMessage('删除角色信息成功');
+    }
+
 }
