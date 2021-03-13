be0dbd6088010cfebfc23a1e67376af543a133bc
linyiyuan <linyiyuann@gmail.com>
2021-03-13 17:58:30 +0800

    删除数据库时区

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -20,7 +20,6 @@ return [
         'charset' => env('DB_CHARSET', 'utf8'),
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
-        'timezone' => 'Asia/Shanghai',
         'pool' => [
             'min_connections' => 1,
             'max_connections' => 10,
